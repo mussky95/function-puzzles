f4a4217f30f6106a749722b245a464efc2375df8
Nikita Mussky <mikita.muski@itechart-group.com>
2019-11-13 19:02:12 +0300

    Even Numbers Average WIP

---
+++ __tests__/even-numbers-average.test.js
@@ -0,0 +1,9 @@
+import {} from '../src/even-numbers-average';
+
+describe('Even numbers average value', () => {
+  test('', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+
+    expect(getEvenNumsAverage(array)).toEqual(4);
+  });
+});
---
+++ src/even-numbers-average.js
@@ -0,0 +1,7 @@
+const getEvenNumsAverage = (array) => {
+
+};
+
+
+
+export { getEvenNumsAverage, };
