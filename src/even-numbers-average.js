e76546c74b6270972408ae00f80f7854fd136b01
Nikita Mussky <mikita.muski@itechart-group.com>
2019-11-20 11:51:02 +0300

    Even numbers average fix

--- src/even-numbers-average.js
+++ src/even-numbers-average.js
@@ -9,7 +9,7 @@ const add = (acc, item) => ({ sum: acc.sum + item, count: acc.count + 1 });
 
 const aggregate = (fn, acc) => (array) => reduce(array, fn, acc);
 
-const average = ({ sum: test, count }) => test / count;
+const average = ({ sum, count }) => sum / count;
 
 const getEvenNumsAverage = pipe(filter(isEven), aggregate(add, { sum: 0, count: 0 }), average);
 
